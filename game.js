64a293292cf544f15553e7f0b86683ecdb8bcab4
Nousheen-Adeel <160176881+Nousheen-Adeel@users.noreply.github.com>
2024-04-23 01:16:01 +0500

    Adding game

---
+++ game.js
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let guessAnswer = Math.floor(Math.random() * 10 + 1);
+let guessnumber = await inquirer.prompt([
+    {
+        type: "number",
+        name: "yourguess",
+        message: "Enter your guess number between 1 & 10: ",
+    }
+]);
+if (guessnumber.yourguess === guessAnswer) {
+    console.log(chalk.bgGreenBright("Congratulation!!!****** You guess the right number******"));
+}
+else if (guessnumber.yourguess > 10) {
+    console.log(chalk.yellowBright("Kindly enter number between 1 to 10"));
+}
+else {
+    console.log(chalk.redBright("Sorry!!! You guess the wrong number"));
+}
